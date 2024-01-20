3320ed34666762d5615309d366be9a7dc03f4029
NyaradzoB <79171484+NyaradzoB@users.noreply.github.com>
2024-01-20 16:52:30 -0700

    initial commit

---
+++ ThatGirl/ThatGirl/ThatGirlApp.swift
@@ -0,0 +1,29 @@
+//
+//  ThatGirlApp.swift
+//  ThatGirl
+//
+//  Created by Nyaradzo Bere on 1/20/24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct ThatGirlApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
